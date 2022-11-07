6738991018f202693bbc86fab383d6e54a7b201e
francisco-solis99 <franciscosoliscss@gmail.com>
2022-11-07 13:08:12 -0600

    [Config] sequelize config file and enviorioments

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // Midlewares
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
