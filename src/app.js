fab9983459d5b353b78306e6d51fd66ae8d8e30a
francisco-solis99 <franciscosoliscss@gmail.com>
2022-11-06 21:38:56 -0600

    [Add] the initial config with the initial dependencies of the project

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+// Midlewares
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Welcome to SounWave API' });
+})
+
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+})
---
+++ src/routes/index.routes.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+
+// Here we will write the routes
+
+module.exports = router
