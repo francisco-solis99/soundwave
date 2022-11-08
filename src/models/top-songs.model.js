f94afb341506775b4e95afc0d892d10bdc2f694c
francisco-solis99 <franciscosoliscss@gmail.com>
2022-11-08 14:23:59 -0600

    Explicit refer of foreign key in topsongs model

--- src/models/top-songs.model.js
+++ src/models/top-songs.model.js
@@ -2,8 +2,40 @@ const { DataTypes} = require('sequelize');
 
 
 const TopsSongs =  (sequelize) => sequelize.define('topSongs', {
-    id: {type: DataTypes.INTEGER, primaryKey: true},
-},);
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    topId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'tops',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+    },
+    songId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'songs',
+            key: 'id'
+        },
+        onDelete: 'CASCADE'
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+}, {
+    hooks: {
+        beforeCreate: function (topSong, options) {
+            topSong.createdAt = new Date();
+            topSong.updatedAt = new Date();
+        },
+        beforeUpdate: function(topSong, options) {
+            topSong.updatedAt = new Date();
+        },
+    },
+});
 
 
 module.exports = TopsSongs;
