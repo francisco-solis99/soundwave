4df5db4f40053ef2bfde81dce0dea460e55217a3
francisco-solis99 <franciscosoliscss@gmail.com>
2022-11-08 14:23:15 -0600

    [Add] explicit refer of foreign keys in song model

--- src/models/songs.models.js
+++ src/models/songs.models.js
@@ -1,21 +1,40 @@
 const { DataTypes } = require('sequelize')
 
 const Songs = (sequelize) => sequelize.define('songs', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     name: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true
     },
     year: {
-        type: DataTypes.NUMBER
+        type: DataTypes.INTEGER
     },
     linkYT: {
         type: DataTypes.TEXT,
-        unique: true,
         validate: {
             isUrl: true
         }
+    },
+    artistId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'artists',
+            key: 'id'
+        },
+        onDelete: 'CASCADE'
+    },
+    genreId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'genres',
+            key: 'id'
+        },
+        onDelete: 'CASCADE'
     },
     createdAt: DataTypes.DATE,
     updatedAt: DataTypes.DATE
