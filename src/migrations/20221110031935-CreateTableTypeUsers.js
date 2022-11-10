b59dcbe7abe3db11b2214273b70d3893c95ccd41
proxyz100 <marcelacontreras100@gmail.com>
2022-11-09 19:36:39 -0800

    Migrations od users and typeusers created

---
+++ src/migrations/20221110031935-CreateTableTypeUsers.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+      await queryInterface.createTable('typeusers', {
+        id: { 
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true 
+        },
+        name: { 
+          type: Sequelize.STRING,
+          allowNull:false,
+        },
+        createdAt: Sequelize.DATE,
+        updatedAt: Sequelize.DATE
+        })
+    }, 
+    
+    down: async(queryInterface, Sequelize) => {
+      await queryInterface.dropTable('typeusers');
+    }
+  };  
---
+++ src/migrations/20221110032002-CreateTableUsers.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async(queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: { 
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true 
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      surname: Sequelize.STRING,
+      nickName: Sequelize.STRING,
+      email: {
+        type: Sequelize.STRING,
+        allowNull:false, 
+        unique: true,
+        validate: {
+          isEmail:true
+        }
+       },
+      password:{
+        type: Sequelize.STRING,
+        allowNull:false 
+      },
+      typeuserId:{
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'typeusers',
+          key: 'id'
+       },
+       onDelete: 'CASCADE' 
+     },
+     createdAt: Sequelize.DATE,
+     updatedAt: Sequelize.DATE,
+    })  
+  }, 
+
+  down: async(queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+}; 
