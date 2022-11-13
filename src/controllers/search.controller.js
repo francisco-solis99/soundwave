35082220dc6037b61635040d711e2119b83abe44
francisco-solis99 <franciscosoliscss@gmail.com>
2022-11-13 01:33:04 -0600

    [Add] search tops and serach artists songs

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -1,6 +1,30 @@
 const sequelize = require('../config/db');
 const { Op } = require('sequelize');
 
+
+async function searchTops(req, res){
+  const { limit } = req.query;
+  const { query } = req.body;
+  // If the query was no gaven
+  if(!query) return res.status(400).json({ message: 'Query required', data: null });
+
+  await sequelize.models.tops.findAll({
+    limit: limit ? Number(limit) : limit,
+    where: {
+      name: {
+        [Op.like]: `%${query}%`
+      }
+    },
+  })
+  .then(results => {
+    return res.status(200).json({data: results});
+  })
+  .catch(err => {
+    res.status(404).json({ message: err.message, data: null })
+  })
+}
+
+
 async function searchSongs(req, res){
   const { limit } = req.query;
   const { query } = req.body;
@@ -33,8 +57,52 @@ async function searchSongs(req, res){
   })
 }
 
+async function searchArtistsSongs(req, res){
+  const { limit } = req.query;
+  const { query } = req.body;
+  // If the query was no gaven
+  if(!query) return res.status(400).json({ message: 'Query required', data: null });
+
+  const artists = await sequelize.models.artists.findAll({
+    limit: limit ? Number(limit) : limit,
+    where: {
+      name: {
+        [Op.like]: `%${query}%`
+      }
+    },
+  });
+  if(!artists) return res.status(404).json({ message: 'Artists not found', data: null});
+  const artistsIds = artists.map((artist) => artist.dataValues.id);
+
+
+  await sequelize.models.songs.findAll({
+    where: {
+      artistId: {
+        [Op.in]: artistsIds
+      }
+    },
+    include: [
+      {
+        model: sequelize.models.artists,
+        attributes: ['name', 'country', 'ytchannel']
+      },
+      {
+        model: sequelize.models.genres,
+        attributes: ['name']
+      }
+    ]
+  })
+  .then(results => {
+    return res.status(200).json({data: results});
+  })
+  .catch(err => {
+    res.status(404).json({ message: err.message, data: null })
+  })
+}
 
 
 module.exports = {
-  searchSongs
+  searchSongs,
+  searchTops,
+  searchArtistsSongs
 };
--- src/routes/search.routes.js
+++ src/routes/search.routes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
 const {
-  searchSongs
+  searchSongs,
+  searchTops,
+  searchArtistsSongs
 } = require('../controllers/search.controller')
 
 router.get('/songs', searchSongs);
+router.get('/tops', searchTops);
+router.get('/artists', searchArtistsSongs);
 
 module.exports = router;
