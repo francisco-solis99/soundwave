61821ef5ae1c9daf5c325966fe4f4c02a60fa3ba
francisco-solis99 <franciscosoliscss@gmail.com>
2022-11-13 01:46:20 -0600

    [Add] search genres songs controller and route

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -101,8 +101,53 @@ async function searchArtistsSongs(req, res){
 }
 
 
+async function searchGenresSongs(req, res){
+  const { limit } = req.query;
+  const { query } = req.body;
+  // If the query was no gaven
+  if(!query) return res.status(400).json({ message: 'Query required', data: null });
+
+  const genres = await sequelize.models.genres.findAll({
+    limit: limit ? Number(limit) : limit,
+    where: {
+      name: {
+        [Op.like]: `%${query}%`
+      }
+    },
+  });
+  if(!genres) return res.status(404).json({ message: 'Genres not found', data: null});
+  const genresIds = genres.map((genre) => genre.dataValues.id);
+  console.log(genresIds);
+
+  await sequelize.models.songs.findAll({
+    where: {
+      genreId: {
+        [Op.in]: genresIds
+      }
+    },
+    include: [
+      {
+        model: sequelize.models.artists,
+        attributes: ['name', 'country', 'ytchannel']
+      },
+      {
+        model: sequelize.models.genres,
+        attributes: ['name']
+      }
+    ]
+  })
+  .then(results => {
+    return res.status(200).json({data: results});
+  })
+  .catch(err => {
+    res.status(404).json({ message: err.message, data: null })
+  })
+}
+
+
 module.exports = {
   searchSongs,
   searchTops,
-  searchArtistsSongs
+  searchArtistsSongs,
+  searchGenresSongs
 };
--- src/routes/search.routes.js
+++ src/routes/search.routes.js
@@ -2,11 +2,13 @@ const router = require('express').Router()
 const {
   searchSongs,
   searchTops,
-  searchArtistsSongs
+  searchArtistsSongs,
+  searchGenresSongs
 } = require('../controllers/search.controller')
 
 router.get('/songs', searchSongs);
 router.get('/tops', searchTops);
 router.get('/artists', searchArtistsSongs);
+router.get('/genres', searchGenresSongs);
 
 module.exports = router;
