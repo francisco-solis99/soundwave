da2fd3c438e5824f4a3f4c58ede77aa630b6077a
francisco-solis99 <franciscosoliscss@gmail.com>
2022-11-13 00:48:31 -0600

    [Change] presissions

--- src/routes/genres.routes.js
+++ src/routes/genres.routes.js
@@ -11,8 +11,8 @@ const permission = require('../middlewares/permission')
 
 router.get('/', getAllGenres)
 router.get('/:id', getGenreById)
-router.post('/', authenticate, permission(1), createGenre)
-router.patch('/:id', authenticate, permission(1), updateGenre)
-router.delete('/:id', authenticate, permission(1), deleteGenre)
+router.post('/', authenticate, permission(1, 2), createGenre)
+router.patch('/:id', authenticate, permission(1, 2), updateGenre)
+router.delete('/:id', authenticate, permission(1, 2), deleteGenre)
 
 module.exports = router
