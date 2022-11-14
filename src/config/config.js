cd12073fc982595325fc9b76f6f528c8549e6aa6
francisco-solis99 <franciscosoliscss@gmail.com>
2022-11-13 20:52:50 -0600

    [Add] other configuration for config sequelize

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,31 @@
 require('dotenv').config()
+// module.exports = {
+//   "development": {
+//     "username": process.env.DB_USER,
+//     "password": process.env.DB_PASSWORD,
+//     "database": process.env.DB_DATABASE,
+//     "host": process.env.DB_HOST,
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": process.env.DB_USER,
+//     "password": process.env.DB_PASSWORD,
+//     "database": process.env.DB_DATABASE,
+//     "host": process.env.DB_HOST,
+//     "dialect": "mysql"
+//   }
+// }
+
+const defaultConfig = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+};
+
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-  }
-}
+  development: defaultConfig,
+  production: Object.assign(defaultConfig, {}),
+};
