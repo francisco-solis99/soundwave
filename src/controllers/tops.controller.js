54c8cb275ad163b6c2c41d34548caccff8297b78
francisco-solis99 <franciscosoliscss@gmail.com>
2022-12-15 12:28:21 -0600

    [Fix] big about getting user when serach tops by user

--- src/controllers/tops.controller.js
+++ src/controllers/tops.controller.js
@@ -46,32 +46,32 @@ async function updateTop(req, res) {
         description: body.description,
         userId: body.userId
     });
-    return res.status(201).json({ message: 'Top updated successfully', data: updatedTop})
+    return res.status(201).json({ message: 'Top updated successfully', data: updatedTop })
 };
 
 //Delete
 
-async function deleteTop(req, res){
-    const {params: {id}} = req;
+async function deleteTop(req, res) {
+    const { params: { id } } = req;
     const top = await sequelize.models.tops.findByPk(id);
-    if(!top){
-        return res.status(404).json({ message: 'Top not found', data: null});
+    if (!top) {
+        return res.status(404).json({ message: 'Top not found', data: null });
     }
     await top.destroy();
-    return res.json({ message: 'Deleted succesfully', data: true});
+    return res.json({ message: 'Deleted succesfully', data: true });
 };
 
 // Get tops by user
-async function getTopsByUserId(req, res){
-    const {params: {id}} = req;
-    const user = await sequelize.models.tops.findByPk(id);
-    if(!user) return res.status(404).json({ message: 'User not found', data: null});
+async function getTopsByUserId(req, res) {
+    const { params: { id } } = req;
+    const user = await sequelize.models.users.findByPk(id);
+    if (!user) return res.status(404).json({ message: 'User not found', data: null });
 
     return await sequelize.models.tops.findAll({
-        where: {userId: id}
+        where: { userId: id }
     })
         .then(tops => res.status(200).json(tops))
-        .catch(err => res.status(404).json({ message: err.message, data: null}));
+        .catch(err => res.status(404).json({ message: err.message, data: null }));
 }
 
 module.exports = {
